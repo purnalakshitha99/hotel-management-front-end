3c79d163ac54fd698de17880c7396829d5e08aef
purnalakshitha99 <purnalakshitha2@gmail.com>
2025-01-13 08:27:24 +0530

    added props pass the testing image name

--- src/components/UserTag.jsx
+++ src/components/UserTag.jsx
@@ -1,8 +1,13 @@
-function UserTag(){
+function UserTag(props){
+
+    console.log(props)
     return(
         <div>
         
-        
+         <a href="#" >
+         <img src="https://th.bing.com/th/id/R.b2b34517339101a111716be1c203f354?rik=e5WHTShSpipi3Q&pid=ImgRaw&r=0" style={{width:"50px"}}  />
+            </a> 
+            <h1>Purna lakshitha</h1>
         </div>
     )
 }
