ca1426b741c2e8163c5f46729305eb60caa9bb57
purnalakshitha99 <purnalakshitha2@gmail.com>
2025-01-13 08:04:42 +0530

    add profile image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,10 @@
+import UserTag from '../components/UserTag.jsx'
+
 function Header(){
      return(
         <header>
             <h1>Hotel Management System</h1>
+            <UserTag/>
         </header>
      )
 }
--- src/components/UserTag.jsx
+++ src/components/UserTag.jsx
@@ -1,8 +1,10 @@
 function UserTag(){
     return(
         <div>
-            <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pngwing.com%2Fen%2Fsearch%3Fq%3Duser%2BAvatar&psig=AOvVaw1RjeFxa_gh_oN3Ggmgefx5&ust=1736821349468000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCMjYlu7R8YoDFQAAAAAdAAAAABAE"   />
+            <img src="https://th.bing.com/th/id/R.b2b34517339101a111716be1c203f354?rik=e5WHTShSpipi3Q&pid=ImgRaw&r=0"   />
             <h1>Purna lakshitha</h1>
         </div>
     )
 }
+
+export default UserTag;
