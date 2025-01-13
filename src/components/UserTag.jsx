bf56c2a81a891b4c791bf801b4b53e4dba6d2c32
purnalakshitha99 <purnalakshitha2@gmail.com>
2025-01-13 08:24:49 +0530

    remove the image in usetag

--- src/components/UserTag.jsx
+++ src/components/UserTag.jsx
@@ -2,10 +2,7 @@ function UserTag(){
     return(
         <div>
         
-         <a href="#" >
-         <img src="https://th.bing.com/th/id/R.b2b34517339101a111716be1c203f354?rik=e5WHTShSpipi3Q&pid=ImgRaw&r=0" style={{width:"50px"}}  />
-            </a> 
-            <h1>Purna lakshitha</h1>
+        
         </div>
     )
 }
