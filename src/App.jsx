9ce30fd96d0a6e7dfcc844c3fc24d241c3256cf2
purnalakshitha99 <purnalakshitha2@gmail.com>
2025-01-14 22:21:34 +0530

    change the app.js previous stage

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,10 @@
 
 function  App(props) {
   return (
-    // <>
-    //  <Header/>
-    // </>
-    <div>
-        
-    <a href="#" >
-    <img src={props.imageLink} style={{width:"50px"}}  />
-       </a> 
-       <h1>Purna lakshitha</h1>
-   </div>
+    <>
+     <Header/>
+    </>
+      
   )
 }
 
