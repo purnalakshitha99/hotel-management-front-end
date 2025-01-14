6accb80b98b77500bda8ccc83d4d884b833f13c3
purnalakshitha99 <purnalakshitha2@gmail.com>
2025-01-14 22:14:38 +0530

    added props using internal html tag inside use dynamic

--- src/components/UserTag.jsx
+++ src/components/UserTag.jsx
@@ -7,7 +7,7 @@ function UserTag(props){
         <div>
         
          <a href="#" >
-         <img src="https://th.bing.com/th/id/R.b2b34517339101a111716be1c203f354?rik=e5WHTShSpipi3Q&pid=ImgRaw&r=0" style={{width:"50px"}}  />
+         <img src={props.imageLink} style={{width:"50px"}}  />
             </a> 
             <h1>Purna lakshitha</h1>
         </div>
