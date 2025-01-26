663eb3c42391108940f15c31aa2740c49bda4890
purnalakshitha99 <purnalakshitha2@gmail.com>
2025-01-26 19:27:28 +0530

    tailwind css configure success

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ function  App(props) {
   return (
     <>
      <Header/>
+
+
     </>
       
   )
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ import "./header.css"
 function Header(){
      return(
         <header>
-            <h1>Hotel Management System</h1>
+            <h1 className='underline'>Hotel Management System</h1>
             <UserTag  imageLink="https://th.bing.com/th/id/R.b2b34517339101a111716be1c203f354?rik=e5WHTShSpipi3Q&pid=ImgRaw&r=0" name="testing"/>
             <DivTest/>
         </header>
