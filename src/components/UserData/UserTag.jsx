2bc93f1ee1e8423150616e2d8791c9bc4ab865a6
purnalakshitha99 <purnalakshitha2@gmail.com>
2025-01-27 23:26:19 +0530

    add absolute tailwind css in mean near relative content follow userdata div

--- src/components/UserData/UserTag.jsx
+++ src/components/UserData/UserTag.jsx
@@ -7,10 +7,10 @@ function UserTag(props){
     const name = props.imageLink;
     console.log(name)
     return(
-        <div className="">
+        <div className="absolute">
         
          <a href="#" >
-         <img src={props.imageLink} />
+         <img className="w-[50px] h-[50px]" src={props.imageLink} />
             </a> 
             <span>Purna lakshitha</span>
         </div>
