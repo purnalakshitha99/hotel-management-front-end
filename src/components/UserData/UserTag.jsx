2f38a2193196f2b7afbe134c3db4d477da3fad1d
purnalakshitha99 <purnalakshitha2@gmail.com>
2025-01-27 23:35:25 +0530

    added flex for usertag component div now row type

--- src/components/UserData/UserTag.jsx
+++ src/components/UserData/UserTag.jsx
@@ -7,7 +7,7 @@ function UserTag(props){
     const name = props.imageLink;
     console.log(name)
     return(
-        <div className="absolute">
+        <div className="absolute right-20 bg-red-100 flex">
         
          <a href="#" >
          <img className="w-[50px] h-[50px]" src={props.imageLink} />
