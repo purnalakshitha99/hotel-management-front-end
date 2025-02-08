83a5b3b065ce72491529a64f5f086d1323184ee5
purnalakshitha99 <purnalakshitha2@gmail.com>
2025-02-08 22:11:04 +0530

    added to the body content components

---
+++ src/components/BodyContent/BodyContent.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+
+function BodyContent(){
+     return(
+        <header className='w-full bg-blue-500 flex h-[150px] relative  p-[20px] '>
+            <h1 className='text-white text-[30px]' >Hotel Management System</h1>
+                   
+        </header>
+     )
+}
+
+export default BodyContent;
