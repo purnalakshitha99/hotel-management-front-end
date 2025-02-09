cb44aa54ac044f16c8755a6b4e28d1df2f101b4d
purnalakshitha99 <purnalakshitha2@gmail.com>
2025-02-09 22:54:57 +0530

    margin top added

--- src/components/BodyContent/BodyContent.jsx
+++ src/components/BodyContent/BodyContent.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function BodyContent(){
      return(
-        <header className='w-full bg-red-500 flex h-[150px] relative  p-[20px] '>
+        <header className='w-full bg-red-500 flex h-[150px] relative  p-[40px] mt-10 '>
         <h1 className='text-white text-[30px]' >Hotel Management System</h1>
         
        
