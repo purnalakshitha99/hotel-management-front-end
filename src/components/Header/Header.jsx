d8333c4bf1435f1513e9c582d619202d6e93e6a9
purnalakshitha99 <purnalakshitha2@gmail.com>
2025-02-12 23:18:51 +0530

    change the header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,9 +4,9 @@ import DivTest from './DivTest.jsx';
 
 function Header(){
      return(
-        <header className='w-full bg-blue-500 flex h-[150px] relative  p-[20px] '>
+        <header className='w-full bg-blue-500 flex h-[150px] relative  p-[20px] mr-2'>
             <h1 className='text-white text-[30px]' >Hotel Management System</h1>
-            <UserTag  imageLink="https://th.bing.com/th/id/R.b2b34517339101a111716be1c203f354?rik=e5WHTShSpipi3Q&pid=ImgRaw&r=0" name="testing"/>
+            <UserTag  imageLink="https://th.bing.com/th/id/R.b2b34517339101a111716be1c203f354?rik=e5WHTShSpipi3Q&pid=ImgRaw&r=0" name="purnalakshitha"/>
            
         </header>
      )
